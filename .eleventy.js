8b37a4fe3d1e770372b68979afb2a8fcda8c43ff
Allen <goodreds@gmail.com>
2020-08-19 18:50:39 +1000

    Initial commit

--- .eleventy.js
+++ .eleventy.js
@@ -1,17 +1,17 @@
 module.exports = (config) => {
-  config.addPassthroughCopy({ public: './' })
+    config.addPassthroughCopy({ public: './' })
 
-  config.setBrowserSyncConfig({
-    files: ['dist/**/*'],
-    open: true,
-  })
+    config.setBrowserSyncConfig({
+        files: ['dist/**/*'],
+        open: true,
+    })
 
-  config.setDataDeepMerge(true)
+    config.setDataDeepMerge(true)
 
-  return {
-    dir: {
-      input: 'src',
-      output: 'dist',
-    },
-  }
+    return {
+        dir: {
+            input: 'src',
+            output: 'dist',
+        },
+    }
 }
